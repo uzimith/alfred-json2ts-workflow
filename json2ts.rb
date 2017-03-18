9a5c64e8e881ae11a332dbe3614f084bd4380fe9
uzimith <uzimith.x9@gmail.com>
2017-03-18 19:40:39 +0900

    support multi-line

--- json2ts.rb
+++ json2ts.rb
@@ -2,21 +2,20 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-STDERR.puts "input: #{ARGV[0]}"
+code = ARGV[0]
+
+STDERR.puts "input: #{code}"
 
 uri = URI.parse('http://json2ts.com/Home/GetTypeScriptDefinition')
 request = Net::HTTP::Post.new(uri)
-request.body = "ns=root&root=root&code=#{ARGV[0]}"
+request.body = "ns=root&root=root&code=#{code}"
 
 response = Net::HTTP.start(uri.hostname, uri.port) do |http|
   http.request(request)
 end
 
-STDERR.puts response
-
 begin
-  json = JSON.parse(response.body, quirks_mode: true).gsub("\r\n", "\n")
-  puts json
+  puts JSON.parse(response.body, quirks_mode: true).gsub("\r\n", "\n")
 rescue StandardError => e
   STDERR.puts "error: #{e.message}"
   STDERR.puts "body: #{response.body}"
